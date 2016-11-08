7c75c2fb5c37fe1fb4aad5f243666aa856dc5a07
boodovskiy <dzak1987@mail.ru>
2016-11-08 15:02:34 +0200

    footer.php +

--- wp-content/themes/realhomes/footer.php
+++ wp-content/themes/realhomes/footer.php
@@ -7,15 +7,15 @@
 
                 <div class="row">
 
-                        <div class="span3">
+                        <div class="span6">
                             <?php if ( ! dynamic_sidebar( 'footer-first-column' ) ) : ?>
                             <?php endif; ?>
                         </div>
 
-                        <div class="span3">
+                       <!-- <div class="span3">
                             <?php if ( ! dynamic_sidebar( 'footer-second-column' ) ) : ?>
                             <?php endif; ?>
-                        </div>
+                        </div>-->
 
                         <div class="clearfix visible-tablet"></div>
 
@@ -76,5 +76,19 @@ if ( ! is_user_logged_in() ) {
 <a href="#top" id="scroll-top"><i class="fa fa-chevron-up"></i></a>
 
 <?php wp_footer(); ?>
+
+<script type="text/javascript">(function(w,doc) {
+if (!w.__utlWdgt ) {
+    w.__utlWdgt = true;
+    var d = doc, s = d.createElement('script'), g = 'getElementsByTagName';
+    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+    s.src = ('https:' == w.location.protocol ? 'https' : 'http')  + '://w.uptolike.com/widgets/v1/uptolike.js';
+    var h=d[g]('body')[0];
+    h.appendChild(s);
+}})(window,document);
+</script>
+<div data-background-alpha="0.0" data-buttons-color="#FFFFFF" data-counter-background-color="#ffffff" data-share-counter-size="12" data-top-button="false" data-share-counter-type="disable" data-share-style="1" data-mode="share" data-like-text-enable="false" data-mobile-view="true" data-icon-color="#ffffff" data-orientation="fixed-right" data-text-color="#000000" data-share-shape="rectangle" data-sn-ids="fb.vk.tw.ok.gp.mr." data-share-size="30" data-background-color="#ffffff" data-preview-mobile="false" data-mobile-sn-ids="fb.vk.tw.wh.ok.vb.mr." data-pid="1447722" data-counter-background-alpha="1.0" data-following-enable="false" data-exclude-show-more="false" data-selection-enable="true" class="uptolike-buttons" ></div>
+
+
 </body>
 </html>
